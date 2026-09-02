7f364d6be38e9da47641a58e75df730e332f35df
kuotsanhsu <learningstud@gmail.com>
2026-09-02 16:58:06 +0800

    398904	31 ms	7.01 Mib cycle_detection: simplified index-based with mmap stdin and edge-scratch bulk write

--- cp/library_checker/cycle_detection.cpp
+++ cp/library_checker/cycle_detection.cpp
@@ -1,174 +1,231 @@
 // make CXXFLAGS='-std=c++23 -g -DDEBUG' cycle_detection && ./cycle_detection
 // make CXXFLAGS='-std=c++23 -O2' cycle_detection &&
 // 	for i in {1..3}; do ./cycle_detection < sample$i.txt; done
-#ifndef DEBUG
+// #define DEBUG
+#ifdef DEBUG
+#	include <algorithm>
+#else
 #	define NDEBUG
 #endif
 
 #include <cassert>
-#include <iterator>
-#include <iostream>
-
-using edge = std::pair<int, int>;
-static_assert(sizeof(edge) == 8);
+#include <utility>
+#include <string_view>
+#include <span>
+#include <charconv>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
 
 /*
-union vertex;
-
-union edge {
+struct edge {
 	struct {
-		union vertex *_Nonnull source;
-		union vertex *_Nonnull target;
+		int *_Nonnull source;
+		int *_Nonnull target;
 	};
 	struct adj_list {
-		struct adj_list *next;
-		union vertex *_Nonnull vertex;
-	} adj_list;
+		adj_list *adj_list_next;
+		int *_Nonnull target;
+	};
 	struct dfs_stack {
-		struct adj_list *adj_list;
-		struct dfs_stack *prev;
-	} dfs_stack;
-};
-static_assert(sizeof(union edge) == 16);
-
-union vertex {
-	struct edge::adj_list *adj_list;
-	union vertex *_Nonnull source;
-};
-static_assert(sizeof(union vertex) == 8);
-
-struct cycle {
-	int length;
-	struct edge::dfs_stack *dfs_stack;
+		adj_list *adj_list_next;
+		dfs_stack *dfs_stack_prev;
+	};
 };
 */
+using edge_t = std::pair<int, int>;
 
 [[nodiscard]] constexpr int source(const int v) noexcept {return -v - 1;}
 
-[[nodiscard]] constexpr edge
-cycle_detection(const int n, const int m, edge edge[], int vertex[]) noexcept {
-	for (int i = 0; i++ != m;) {
-		const int u = edge[i].first;
-		edge[i].first = vertex[u];
-		vertex[u] = i;
-	}
-	for (int dfs_stack = 0, i = 0; i != n; ++i) for (int v = i, u = v;;) {
-		if (vertex[v] == 0) {
-			if (dfs_stack == 0) break;
-			const int adj_list = edge[dfs_stack].first;
-			// TODO: free dfs_stack
-			if (adj_list == 0) {
-				v = u;
-				assert(vertex[u] < 0);
-				u = source(vertex[u]);
-				vertex[v] = 0;
-				dfs_stack = edge[dfs_stack].second;
-			} else {
-				v = edge[adj_list].second;
-				edge[adj_list].second = edge[dfs_stack].second;
-				dfs_stack = adj_list;
-			}
-		} else if (vertex[v] > 0) {
-			const int adj_list = vertex[v];
-			vertex[v] = source(u);
-			u = v;
-			v = edge[adj_list].second;
-			edge[adj_list].second = dfs_stack;
-			dfs_stack = adj_list;
-		} else {
-			vertex[v] = source(u);
-			const int root = u;
-			int cycle_length = 0, q = 0, p = dfs_stack;
-			do {
-				++cycle_length;
-				const int r = edge[p].second;
-				edge[p].second = q;
-				q = p;
-				p = r;
-				u = source(vertex[u]);
-			} while (u != root);
-			// TODO: for (; p; p = p->prev) { /* free p */ }
-			assert(edge[dfs_stack].second == 0);
-			edge[dfs_stack].second = q;
-			return {cycle_length, dfs_stack};
-		}
-	}
-	return {};
-}
-
-[[nodiscard]] constexpr bool
-cyclic(const int n, const int m, edge edge[], int vertex[],
-	std::input_iterator auto pair, std::output_iterator<int> auto output
-) noexcept {
+[[nodiscard]] constexpr int cycle_detection(edge_t edge[], int vertex[]) noexcept {
+	const auto [n, m] = edge[0];
 	assert(2 <= n);
 	assert(1 <= m);
 	for (int i = 0; i++ != m;) {
-		const ::edge p = *pair++;
-		const auto [u, v] = p;
+		auto &[u, v] = edge[i];
 		assert(0 <= u && u < n);
 		assert(0 <= v && v < n);
 		assert(u != v);
-		edge[i] = {u, v};
+		u = std::exchange(vertex[u], i);
 	}
-
-	auto [cycle_length, dfs_stack] = cycle_detection(n, m, edge, vertex);
-	if (cycle_length == 0) {
-		assert(dfs_stack == 0);
-		*output++ = -1;
-		return false;
+	for (int dfs_stack = 0, i = 0; i != n; ++i) for (int v = i, u = v;;) {
+		assert(0 <= u);
+		assert(0 <= v);
+		if (vertex[v] == 0) {
+			if (dfs_stack == 0) break;
+			const auto [adj_list, dfs_stack_prev] = edge[dfs_stack];
+			// TODO: free dfs_stack
+			if (adj_list == 0) {
+				v = u;
+				u = source(vertex[u]);
+				vertex[v] = 0;
+				dfs_stack = dfs_stack_prev;
+			} else {
+				v = std::exchange(edge[adj_list].second, dfs_stack_prev);
+				dfs_stack = adj_list;
+			}
+		} else if (vertex[v] > 0) {
+			const int adj_list = std::exchange(vertex[v], source(u));
+			u = v;
+			v = std::exchange(edge[adj_list].second, dfs_stack);
+			dfs_stack = adj_list;
+		} else {
+			edge[0] = {0, dfs_stack};
+			int cycle_length = 1;
+			for (;u != v; u = source(vertex[u])) ++cycle_length;
+			return cycle_length;
+		}
 	}
-	assert(dfs_stack != 0);
-
-	*output++ = cycle_length;
-	int p = dfs_stack;
-	do {
-		p = edge[p].second;
-		*output++ = p - 1;
-		--cycle_length;
-	} while (p != dfs_stack);
-	assert(cycle_length == 0);
-	return true;
+	return 0;
 }
 
-namespace std {
-istream& operator>>(istream &is, edge &p) {return is >> p.first >> p.second;}
+[[nodiscard]] std::string_view read_file(const int fd) noexcept {
+	constexpr int mmap_flags = MAP_SHARED
+#ifdef MAP_POPULATE
+		| MAP_POPULATE
+#endif
+#ifdef MAP_RESILIENT_CODESIGN
+		| MAP_RESILIENT_CODESIGN
+#endif
+#ifdef MAP_RESILIENT_MEDIA
+		| MAP_RESILIENT_MEDIA
+#endif
+	;
+	struct ::stat stat;
+	if (::fstat(fd, &stat) == -1) return {};
+	const std::size_t data_size = stat.st_size;
+	if (data_size == 0) return {};
+	void *const data = ::mmap(nullptr, data_size, PROT_READ, mmap_flags, fd, 0);
+	if (data == MAP_FAILED) return {};
+	::madvise(data, data_size, MADV_SEQUENTIAL); // hint-only; don't care about failure
+	return {static_cast<const char*>(data), data_size};
+}
+
+void read_ints(std::span<int> is, std::string_view s) noexcept {
+	auto i = is.begin();
+	bool fresh = true; // -Wunused-but-set-variable MUST warn if NDEBUG
+	for (int n = 0; const char c : s) {
+		if ('0' <= c) {
+			assert(c <= '9');
+			n *= 10;
+			n += c - '0';
+			fresh = false;
+		} else [[unlikely]] {
+			assert(!fresh);
+			assert(c == '\n' || c == ' ');
+			assert(i < is.end());
+			*i++ = n;
+			n = 0;
+			fresh = true;
+		}
+	}
+	assert(fresh || i++ < is.end()); // if (!fresh) assert(i++ < is.end());
+	assert(i <= is.end());
+	assert(is.begin() + 1 < i);
+	const int m = is[1]; // -Wunused-but-set-variable MUST warn if NDEBUG
+	assert(is.begin() + 2 * (m + 1) == i);
+}
+
+class write_ints {
+	char *first, *c, *const last;
+public:
+	constexpr write_ints(std::span<char> buffer) noexcept
+		: first(buffer.data()), c(first), last(first + buffer.size()) {}
+
+	[[nodiscard]] constexpr bool operator()(const int value) noexcept {
+		const auto [ptr, ec] = std::to_chars(c, last, value);
+		if (ec == std::errc::value_too_large) [[unlikely]] {
+			assert(ptr == last);
+			return false;
+		}
+		assert(ec == std::errc());
+		c = ptr;
+		assert(c < last);
+		*c++ = '\n';
+		return true;
+	}
+
+	[[nodiscard]] int flush() noexcept {
+		assert(c <= last);
+		while (first != c) {
+			assert(first < c);
+			const ::ssize_t nbyte = ::write(STDOUT_FILENO, first, c - first);
+			if (nbyte == -1) [[unlikely]] {
+				if (errno == EINTR) continue;
+				assert(errno != 0);
+				return errno;
+			}
+			assert(nbyte > 0);
+			first += nbyte;
+		}
+		return 0;
+	}
+};
+
+[[nodiscard]] constexpr std::size_t total_digits_before(std::size_t upper_bound) noexcept {
+	if (upper_bound == 0) return 0;
+	std::size_t total = 1, digits = 1, a = 1, b = 10;
+	while (b <= upper_bound) {
+		assert(a <= b);
+		total += (b - a) * digits;
+		a = b;
+		b *= 10; // FIXME: infinite loop if b overflows
+		++digits;
+	}
+	assert(a <= upper_bound);
+	return total + (upper_bound - a) * digits;
 }
 
 int main() {
-	std::cin.tie(nullptr)->sync_with_stdio(false);
-	constexpr int max_size = 500'000;
-	static struct {
-		::edge edge[max_size + 1];
-		int vertex[max_size]{};
-	} zone;
-	std::ostream_iterator<int> output(std::cout, "\n");
-	std::istream_iterator<edge> pair(std::cin);
-	const auto [n, m] = *pair++;
-	(void)cyclic(n, m, zone.edge, zone.vertex, pair, output);
+	constexpr std::size_t max_size = 500'000, edge_size = max_size + 1;
+	static constinit union {
+		edge_t edge[edge_size];
+		int is[edge_size * 2];
+		char output[edge_size * 8];
+	} storage{};
+	static_assert(sizeof(storage.edge) == sizeof(storage.is));
+	static_assert(sizeof(storage.is) == sizeof(storage.output));
+	constexpr std::size_t total_digits = total_digits_before(500'000);
+	static_assert(total_digits == 2'888'890);
+	static_assert(sizeof(storage.output) >= 6 + 1 + total_digits + max_size);
+	read_ints(storage.is, read_file(STDIN_FILENO));
+	edge_t *edge = storage.edge;
+	const auto [n, m] = *edge;
+	static constinit int vertex[max_size]{};
+	const int cycle_length = cycle_detection(edge, vertex);
+	write_ints println(storage.output);
+	if (cycle_length == 0) {
+		assert(*edge == std::make_pair(n, m));
+		if (!println(-1)) return 1;
+	} else {
+		assert(0 < cycle_length && cycle_length <= std::min(n, m));
+		for (int dfs_stack = edge->second, i = 0; i != cycle_length; ++i) {
+			assert(0 < dfs_stack && dfs_stack <= m);
+			vertex[i] = dfs_stack;
+			dfs_stack = edge[dfs_stack].second;
+		}
+		if (!println(cycle_length)) return 1;
+		for (int i = cycle_length; i-- != 0;) if (!println(vertex[i] - 1)) return 1;
+	}
+	if (println.flush() != 0) return 2;
+	return 0;
 }
 
-template<typename T> struct noop_output_iterator {
-	using iterator_category = std::output_iterator_tag;
-	using difference_type = std::ptrdiff_t;
-	constexpr noop_output_iterator& operator=(const T&) noexcept {return *this;}
-	constexpr noop_output_iterator& operator=(T&&) noexcept {return *this;}
-	constexpr noop_output_iterator& operator*() noexcept {return *this;}
-	constexpr noop_output_iterator& operator++() noexcept {return *this;}
-	constexpr noop_output_iterator operator++(int) const noexcept {return *this;}
-};
-
-static_assert(std::output_iterator<noop_output_iterator<int>, int>);
-
-template<int n, int m> [[nodiscard]] constexpr bool
-sample(const std::array<edge, m> pair) noexcept {
-	struct {
-		::edge edge[m + 1];
-		int vertex[n]{};
-	} zone;
-	noop_output_iterator<int> output;
-	return cyclic(n, m, zone.edge, zone.vertex, pair.begin(), output);
+#ifdef DEBUG
+template<int n, int m>
+[[nodiscard]] constexpr bool cyclic(std::span<const edge_t, m> pair) noexcept {
+	edge_t edge[m + 1] = {{n, m},};
+	std::ranges::copy(pair, edge + 1);
+	int vertex[n]{};
+	return cycle_detection(edge, vertex) != 0;
 }
-
-static_assert(sample<5, 7>(std::array<edge, 7>{{{0, 3}, {0, 4}, {4, 2}, {4, 3}, {4, 0}, {2, 1}, {1, 0}}}));
-static_assert(!sample<2, 1>(std::array<edge, 1>{{{1, 0}}}));
-static_assert(sample<4, 6>(std::array<edge, 6>{{{0, 1}, {1, 2}, {2, 0}, {0, 1}, {1, 3}, {3, 0}}}));
+static_assert(cyclic<5, 7>({{
+	{0, 3}, {0, 4}, {4, 2}, {4, 3}, {4, 0}, {2, 1}, {1, 0},
+}}));
+static_assert(!cyclic<2, 1>({{
+	{1, 0},
+}}));
+static_assert(cyclic<4, 6>({{
+	{0, 1}, {1, 2}, {2, 0}, {0, 1}, {1, 3}, {3, 0},
+}}));
+#endif
