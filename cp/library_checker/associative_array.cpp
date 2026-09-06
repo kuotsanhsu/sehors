6d52c8f33501bd8c65e38ffbca64505f118a74e6
kuotsanhsu <learningstud@gmail.com>
2026-09-06 14:50:36 +0800

    399995	673 ms	61.51 Mib associative_array: std::map iostream

---
+++ cp/library_checker/associative_array.cpp
@@ -0,0 +1,35 @@
+#pragma GCC optimize "Ofast"
+
+#ifndef	DEBUG
+#define	NDEBUG
+#endif
+
+#include <cassert>
+#include <iostream>
+#include <map>
+
+int main() {
+	std::cin.tie(nullptr)->sync_with_stdio(false);
+	constexpr long long query_limit = 1'000'000;
+	constexpr long long key_value_limit = 1'000'000'000'000'000'000;
+	int Q;
+	std::cin >> Q;
+	assert(1 <= Q and Q <= query_limit);
+	for (std::map<long long, long long> m; Q--;) {
+		char c;
+		long long k;
+		std::cin >> c >> k;
+		assert(0 <= k and k <= key_value_limit);
+		if (c == '0') {
+			long long v;
+			std::cin >> v;
+			assert(0 <= v and v <= key_value_limit);
+			m[k] = v;
+		} else {
+			assert(c == '1');
+			const auto node = m.find(k);
+			if (node == m.end()) std::cout << "0\n";
+			else std::cout << node->second << '\n';
+		}
+	}
+}
